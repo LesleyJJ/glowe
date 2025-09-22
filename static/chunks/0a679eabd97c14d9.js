3083cd49c47ac9f937232fd4122035bee9d4a27e
LesleyJJ <jacobjohnlesley@gmail.com>
2025-09-22 16:39:19 +0530

    build_v1.0

---
+++ static/chunks/0a679eabd97c14d9.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/feb7ba503e18da6f.js",
+  "static/chunks/turbopack-ed815a7ace968a23.js"
+])
---
+++ static/chunks/a3a109a5575d86f5.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/feb7ba503e18da6f.js",
+  "static/chunks/turbopack-c2be3e9d6c52c5ba.js"
+])
